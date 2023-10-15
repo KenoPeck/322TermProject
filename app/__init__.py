c3eb34e17cb977d91e6c5489f020e7825bbabd20
CooperPage226 <cooper.page@wsu.edu>
2023-10-15 16:15:18 -0700

    added all the base files to start the project

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,26 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 ##Create application
-def create_app(config_class = Config)
+def create_app(config_class = Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    app.static_folder = config_class.STATIC_FOLDER 
+    app.template_folder = config_class.TEMPLATE_FOLDER
+
+    db.init_app(app)
+    # TODO: (milestone 3) Configure the app object for login using `init_app` function. 
+    # TODO: (milestone 3) Configure the app object for moment using `init_app` function. 
+
+    # blueprint registration
+    from app.Controller.errors import bp_errors as errors
+    app.register_blueprint(errors)
+    from app.Controller.auth_routes import bp_auth as auth
+    app.register_blueprint(auth)
+    from app.Controller.routes import bp_routes as routes
+    app.register_blueprint(routes)
+
+    if not app.debug and not app.testing:
+        pass
+        # ... no changes to logging setup
+
+    return app
---
+++ termProject.py
@@ -0,0 +1,16 @@
+from app import create_app, db
+from app.Model.models import Post
+
+app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': app.db, 'Post': Post, 'User': User}
+
+@app.before_request
+def initDB(*args, **kwargs):
+    if app.got_first_request:
+        db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
